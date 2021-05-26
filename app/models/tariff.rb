71ee103d31ec149fced4ba80655ccda57e0db5f3
mximenes <ximenesmurilo@gmail.com>
2021-05-26 20:06:41 -0300

    some tests in tariff model

--- app/models/tariff.rb
+++ app/models/tariff.rb
@@ -1,2 +1,10 @@
 class Tariff < ApplicationRecord
+    validates :tusd_k, numericality: true
+    validates :grupo , format: { with: /\A[a-zA-Z]+\z/ }, length: { maximum: 1 }, inclusion: { in: %w(A B),
+        message: "%{value} is not a valid group" }
+
+    def some_output
+        "tusd_m = #{self.tusd_m}"
+    end
+
 end
--- test/models/tariff_test.rb
+++ test/models/tariff_test.rb
@@ -4,4 +4,28 @@ class TariffTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "tariff tusd_k not string" do
+    tariff = Tariff.new
+    tariff.tusd_k = "string" # Only numbers
+    tariff.grupo = 12 # Only 1 length string [A,B]
+    assert_not tariff.save, "Save tusd_k string and grupo numeric"
+  end
+
+  test "should report error" do
+    tariff = Tariff.new
+    tariff.tusd_m = 3.0
+    tariff.save
+    assert_equal tariff.some_output, "tusd_m = #{tariff.tusd_m}"
+  end
+
+  test "te float value" do
+    tariff = Tariff.new
+    tariff.tusd_k = 2.23
+    tariff.grupo = "A"
+    tariff.te = "test"
+    assert tariff.save
+  end
+
+
 end
