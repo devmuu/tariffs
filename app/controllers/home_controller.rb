a4a31b28869e90b374b1bc485d291a9415fa0c68
mximenes <ximenesmurilo@gmail.com>
2021-06-30 12:12:08 -0300

    add scope in rate model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @min_year = Rate.min_year
+    @max_year = Rate.max_year
+    @min_month = Rate.min_month
+    @max_month = Rate.max_month
   end
 
   def table
--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,2 +1,6 @@
 class Rate < ApplicationRecord
+    scope :min_year, -> { minimum('ano').year }
+    scope :max_year, -> { maximum('ano').year }
+    scope :min_month, -> { where('ano = ?', minimum('ano')).minimum('mes')}
+    scope :max_month, -> { where('ano = ?', maximum('ano')).maximum('mes')}
 end
